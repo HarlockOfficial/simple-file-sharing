7ba20d70e5240cec3af53de7fb3de25d2bcb588e
HarlockOfficial <harlockofficial.github@gmail.com>
2022-06-20 13:11:51 +0200

    [edit] auth_token_database.h rename functions to avoid conflicts [edit] auth_token_database.c added lock2 to improve mutex security, updated methods to support uuid, minor fix

--- server/storage/database_wrapper/auth_token/auth_token_database.c
+++ server/storage/database_wrapper/auth_token/auth_token_database.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
-#include <string.h>
 #include <pthread.h>
+#include <uuid/uuid.h>
 #include "../../../../utils/file_manager/file_manager.h"
 #include "auth_token_database.h"
 
 #define FILENAME "auth_token.bin"
 #define TEMP_FILENAME "tmp_auth_token.bin"
 
-pthread_mutex_t lock;
+pthread_mutex_t lock, lock2;
 
 long long find_by_id(auth_token *data);
 
 long long find_by_token(auth_token *data);
 
-long long create_entry(auth_token *data){
+long long create_entry_auth_token(auth_token *data){
+    pthread_mutex_lock(&lock2);
     pthread_mutex_lock(&lock);
-    if(strcmp(data->token, "") == 0){
+    uuid_t empty_uuid;
+    uuid_clear(empty_uuid);
+    if(uuid_compare(data->token, empty_uuid) == 0){
+        pthread_mutex_unlock(&lock);
+        pthread_mutex_unlock(&lock2);
         return -1;
     }
     if(data->user_id <= 0){
+        pthread_mutex_unlock(&lock);
+        pthread_mutex_unlock(&lock2);
         return -2;
     }
     long long position = file_size(FILENAME);
@@ -30,34 +37,41 @@ long long create_entry(auth_token *data){
             printf("Error reading file %s element number %lld\n", FILENAME, i);
             continue;
         }
-        if(strcmp(token.token, data->token) == 0){
+        if(uuid_compare(token.token, data->token) == 0){
+            pthread_mutex_unlock(&lock);
+            pthread_mutex_unlock(&lock2);
             return -3;
         }
         if(token.user_id == data->user_id){
             pthread_mutex_unlock(&lock);
-            delete_entry(&token);
-            pthread_mutex_lock(&lock);
+            pthread_mutex_unlock(&lock2);
+            return -4;
         }
     }
     data->token_id = token.token_id+1;
     file_write(FILENAME, sizeof(auth_token), position,data);
     pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&lock2);
     return position;
 }
 
-long long search_entry(auth_token *data){
+long long search_entry_auth_token(auth_token *data){
     pthread_mutex_lock(&lock);
+    long long output = -1;
+    uuid_t empty_uuid;
+    uuid_clear(empty_uuid);
     if(data->token_id>0){
-        return find_by_id(data);
-    }if(strcmp(data->token, "") != 0){
-        return find_by_token(data);
+        output = find_by_id(data);
+    }if(uuid_compare(data->token, empty_uuid) != 0){
+        output = find_by_token(data);
     }
     pthread_mutex_unlock(&lock);
-    return -1;
+    return output;
 }
 
-int delete_entry(auth_token *data){
-    long long index = search_entry(data);
+int delete_entry_auth_token(auth_token *data){
+    pthread_mutex_lock(&lock2);
+    long long index = search_entry_auth_token(data);
     pthread_mutex_lock(&lock);
     if(index<=0){
         return -1;
@@ -74,6 +88,7 @@ int delete_entry(auth_token *data){
     delete_file(FILENAME);
     rename_file(TEMP_FILENAME, FILENAME);
     pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&lock2);
     return 0;
 }
 
@@ -86,7 +101,7 @@ long long find_by_token(auth_token *data) {
             printf("Error reading file %s element number %lld\n", FILENAME, i);
             continue;
         }
-        if(strcmp(token.token, data->token) == 0){
+        if(uuid_compare(token.token, data->token) == 0){
             *data=token;
             return i;
         }
--- server/storage/database_wrapper/auth_token/auth_token_database.h
+++ server/storage/database_wrapper/auth_token/auth_token_database.h
@@ -1,7 +1,7 @@
 #ifndef SIMPLE_FILE_SHARING_AUTH_TOKEN_DATABASE_H
 #define SIMPLE_FILE_SHARING_AUTH_TOKEN_DATABASE_H
-#include "../../../../utils/tables/auth_token.h"
-long long create_entry(auth_token *data);
-long long search_entry(auth_token *data);
-int delete_entry(auth_token *data);
+#include "../../../../utils/tables/auth_token/auth_token.h"
+long long create_entry_auth_token(auth_token *data);
+long long search_entry_auth_token(auth_token *data);
+int delete_entry_auth_token(auth_token *data);
 #endif //SIMPLE_FILE_SHARING_AUTH_TOKEN_DATABASE_H
